55fa22ed9893070c6c66900107ade1830cf43aa7
Ezra Black <dev.ezrablack@gmail.com>
2020-05-18 11:42:21 -0500

    Added test for accurate network speed.

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -22,4 +22,29 @@ class iTunes_SearchTests: XCTestCase {
         XCTAssertGreaterThan(controller.searchResults.count, 0)
     }
     
+    
+    
+    func testSpeedOfTypicalRequest() {
+        measure {
+            let expectation = self.expectation(description: "Wait for results")
+            let controller = SearchResultController()
+            controller.performSearch(for: "GarageBand", resultType: .software) {
+                expectation.fulfill()
+            }
+            wait(for: [expectation], timeout: 5)
+        }
+    }
+    
+    func testSpeedOfTypicalRequestMoreAccurately() {
+        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+            let expectation = self.expectation(description: "Wait for results")
+            let controller = SearchResultController()
+            startMeasuring()
+            controller.performSearch(for: "GarageBand", resultType: .software) {
+                self.stopMeasuring()
+                expectation.fulfill()
+            }
+            wait(for: [expectation], timeout: 5)
+        }
+    }
 }
