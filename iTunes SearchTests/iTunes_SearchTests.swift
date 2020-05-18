d69bc165d2e5c27c1945d9db4f8f9a1429830a70
Ezra Black <dev.ezrablack@gmail.com>
2020-05-18 11:27:09 -0500

    added an expectation to the previous test to ensure it works properly

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -7,27 +7,19 @@
 //
 
 import XCTest
-
+@testable import iTunes_Search
 class iTunes_SearchTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testForSomeResults() {
+        let expectation = self.expectation(description: "Wait for results")
+        let controller = SearchResultController()
+        controller.performSearch(for: "GarageBand", resultType: .software) {
+            print("⚠️Got Results Back.⚠️")
+            XCTAssertGreaterThan(controller.searchResults.count, 0)
+            expectation.fulfill()
         }
+        wait(for: [expectation], timeout: 5)
+        XCTAssertGreaterThan(controller.searchResults.count, 0)
     }
-
+    
 }
