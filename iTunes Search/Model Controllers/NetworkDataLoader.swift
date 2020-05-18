5acc0c6287ccd21914d2825c5830a7de48fde55b
Ezra Black <dev.ezrablack@gmail.com>
2020-05-18 11:49:44 -0500

    added URL extension that implements the network data loader

--- iTunes Search/Model Controllers/NetworkDataLoader.swift
+++ iTunes Search/Model Controllers/NetworkDataLoader.swift
@@ -12,3 +12,12 @@ import Foundation
 protocol NetworkDataLoader {
     func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
 }
+
+extension URLSession: NetworkDataLoader {
+    func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask(with: request) { (data, response, error) in
+            completion(data, response, error)
+        }.resume()
+    }
+}
+
